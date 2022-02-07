e89c281e996aaac8669df0df66fbefec704f0588
Qifeng Chen <chenqifeng22@gmail.com>
2022-02-08 00:49:46 +0800

    update the URLs to download the models

--- download_models.py
+++ download_models.py
@@ -1,54 +1,20 @@
-import requests
-
-def download_file_from_google_drive(id, destination):
-    URL = "https://docs.google.com/uc?export=download"
-
-    session = requests.Session()
-
-    response = session.get(URL, params = { 'id' : id }, stream = True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
-
-    save_response_content(response, destination)    
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
+import urllib.request
 
 print('Dowloading VGG-19 Model (510Mb)')
-download_file_from_google_drive('0B_B_FOgPxgFLRjdEdE9NNTlzUWc', 'VGG_Model/imagenet-vgg-verydeep-19.mat')
+urllib.request.urlretrieve('https://cqf.io/data/Tensorflow_models/VGG_Model/imagenet-vgg-verydeep-19.mat','VGG_Model/imagenet-vgg-verydeep-19.mat')
 
 print('Dowloading CRN 1024p Model (500Mb)')
-download_file_from_google_drive('0B_B_FOgPxgFLLXdaaU5yR0ZKaU0', 'result_1024p/model.ckpt.data-00000-of-00001')
-download_file_from_google_drive('0B_B_FOgPxgFLVlhkTmJUbDZNdDg', 'result_1024p/model.ckpt.meta')
+urllib.request.urlretrieve('https://cqf.io/data/Tensorflow_models/result_1024p/model.ckpt.data-00000-of-00001','result_1024p/model.ckpt.data-00000-of-00001')
+urllib.request.urlretrieve('https://cqf.io/data/Tensorflow_models/result_1024p/model.ckpt.meta','result_1024p/model.ckpt.meta')
 
 print('Dowloading CRN 512p Model (1.2Gb)')
-download_file_from_google_drive('0B_B_FOgPxgFLNjBMRjZWNjVyS1E', 'result_512p/model.ckpt.data-00000-of-00001')
-download_file_from_google_drive('0B_B_FOgPxgFLdENlSE9zbjJTbjg', 'result_512p/model.ckpt.meta')
+urllib.request.urlretrieve('https://cqf.io/data/Tensorflow_models/result_512p/model.ckpt.data-00000-of-00001','result_512p/model.ckpt.data-00000-of-00001')
+urllib.request.urlretrieve('https://cqf.io/data/Tensorflow_models/result_512p/model.ckpt.meta','result_512p/model.ckpt.meta')
 
 print('Dowloading CRN 256p Model (1.2Gb)')
-download_file_from_google_drive('0B_B_FOgPxgFLLUVQTEg0alRWNDQ', 'result_256p/model.ckpt.data-00000-of-00001')
-download_file_from_google_drive('0B_B_FOgPxgFLM0NjR2QxSUg5SWM', 'result_256p/model.ckpt.meta')
+urllib.request.urlretrieve('https://cqf.io/data/Tensorflow_models/result_256p/model.ckpt.data-00000-of-00001','result_256p/model.ckpt.data-00000-of-00001')
+urllib.request.urlretrieve('https://cqf.io/data/Tensorflow_models/result_256p/model.ckpt.meta','result_256p/model.ckpt.meta')
 
 print('Downloading GTA 256p Model (1.2Gb)')
-download_file_from_google_drive('0B_B_FOgPxgFLTVl2MWxpOGtzczA','result_GTA/model.ckpt.data-00000-of-00001')
-download_file_from_google_drive('0B_B_FOgPxgFLSThjb2hzSHM2Qzg','result_GTA/model.ckpt.meta')
-
-print('Downloading GTA 256p Model (1.2Gb)')
-download_file_from_google_drive('0B_B_FOgPxgFLTVl2MWxpOGtzczA','result_GTA/model.ckpt.data-00000-of-00001')
-download_file_from_google_drive('0B_B_FOgPxgFLSThjb2hzSHM2Qzg','result_GTA/model.ckpt.meta')
-
+urllib.request.urlretrieve('https://cqf.io/data/Tensorflow_models/result_GTA/model.ckpt.data-00000-of-00001','result_GTA/model.ckpt.data-00000-of-00001')
+urllib.request.urlretrieve('https://cqf.io/data/Tensorflow_models/result_GTA/model.ckpt.meta','result_GTA/model.ckpt.meta')
